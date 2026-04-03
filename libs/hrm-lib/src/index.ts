a849bbde6ebc9f5fd7855c7f81a18c5fa4a5ca24
Arthur <archi.sb.pochta@gmail.com>
2026-04-03 20:41:47 +0300

    init

--- libs/hrm-lib/src/index.ts
+++ libs/hrm-lib/src/index.ts
@@ -2,3 +2,5 @@ export * from './lib/shared/tabs/tabs.component';
 export * from './lib/shared/input/input.component';
 export * from './lib/shared/button/button.component';
 export * from './lib/shared/button/models/button-variant.model';
+
+export * from './lib/shared/sidebar/sidebar.component';
